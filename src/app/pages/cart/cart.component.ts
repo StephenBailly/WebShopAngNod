534687474469d617cc8136e49ba19fd30c9074bc
BAILLY <stephen.bailly@capgemini.com>
2023-03-21 14:32:57 +0100

    fixing cart

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -8,20 +8,41 @@ import { Cart, CartItem } from 'src/app/models/cart.model';
 
 export class CartComponent implements OnInit {
     cart: Cart = { items: [{
-
         product: 'https://via.placeholder.com/150',
         name: 'sneakers',
         price: 150,
         quantity: 1,
         id: 1,
-    }]};
+    },
+    {
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 150,
+        quantity: 2,
+        id: 2,
+        }
+    ]};
 
-    datasource: Array<CartItem> = [];
+    dataSource: Array<CartItem> = [];
+    displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+    ];
 
   constructor() { }
 
   ngOnInit(): void {
-  this.datasource = this.cart.items;
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.
+      map((item) => item.price * item.quantity)
+       .reduce((prev, current) => prev+current, 0);
   }
 
 }
